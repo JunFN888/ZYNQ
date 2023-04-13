4ca920f252438c726108151adb900fe1eed2cf1a
JunFN888 <junfengwang8888@163.com>
2023-04-13 15:49:47 +0800

    AXU3EG1.0

---
+++ AXU3EG/source/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ZYNQ MPSoC开发平台FPGA教程'
+copyright = '2023, ALINX       http://www.alinx.com'
+author = 'JunFN'
+release = '1.0'
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+#extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+language = 'zh_CN'
+#language = 'en'
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'alabaster'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'groundwork'
+#html_theme = 'nature'
+html_static_path = ['_static']
---
+++ AXU3EG/source/index.rst
@@ -0,0 +1,65 @@
+.. ZYNQ MPSoC 开发平台 FPGA 教程 documentation master file, created by
+   sphinx-quickstart on Mon Mar 20 10:54:33 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+.. _ALINX: http://www.alinx.com
+
+
+`ALINX`_ ZYNQ MPSoC 开发平台 FPGA 教程
+======================================
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: 公司简介
+
+   src/about_alinx
+
+.. toctree::
+   :maxdepth: 2
+   :caption: AXU3EG型号板卡教程
+   
+   src/introduction
+   src/Ultrascale_MPSoC
+   src/Hardware_introduction
+   src/verilog_base
+   src/pl_led
+   src/pll
+   src/ram
+   src/rom
+   src/fifo
+   src/key
+   src/pwm
+   src/uart
+   src/rs485
+   src/pl_ddr4
+   src/hdmi_out
+   src/hdmi_char
+   src/7lcd
+   src/ad7606
+   src/ad9238_d
+   src/adda
+   src/ad9767_d
+
+.. toctree::
+   :maxdepth: 1
+   :caption: AXU3EG常见问题
+   
+   src/problem
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Alinx版权
+
+   src/IP
+
+
+
+
+Alinx serve
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
